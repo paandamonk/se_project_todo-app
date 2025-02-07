d77c1ddf904414f01be9e04708b99169509c1790
Skye Emery <paandamonk@gmail.com>
2025-02-07 15:00:08 -0500

    Added FormValidator class, removed validate.js and scripts folder. Added Todo class, moved code from index.js to Todo.js. added utils/constants, moved constants out of index.js into constants.js.

---
+++ components/FormValidator.js
@@ -0,0 +1,81 @@
+export default class FormValidator {
+    constructor(settings, form) {
+        this._settings = settings;
+        this._form = form;
+    }
+
+
+
+    resetValidation() {
+        this._buttonElement.disabled = true;
+        this._buttonElement.classList.add(this._settings.inactiveButtonClass);
+        this._form.reset();
+    }
+
+    _showInputError(inputElement, errorMessage) {
+        this._errorElementId = `#${inputElement.id}-error`;
+        this._errorElement = this._formElement.querySelector(this._errorElementId);
+        inputElement.classList.add(this._settings.inputErrorClass);
+        this._errorElement.textContent = errorMessage;
+        this._errorElement.classList.add(this._settings.errorClass);
+    }
+
+    _hideInputError(inputElement) {
+        this._errorElementId = `#${inputElement.id}-error`;
+        this._errorElement = this._formElement.querySelector(this._errorElementId);
+        inputElement.classList.remove(this._settings.inputErrorClass);
+        this._errorElement.classList.remove(this._settings.errorClass);
+        this._errorElement.textContent = "";
+    }
+
+    _checkInputValidity(inputElement) {
+        if (!inputElement.validity.valid) {
+            this._showInputError(inputElement, inputElement.validationMessage);
+          } else {
+            this._hideInputError(inputElement);
+          }
+    }
+
+    _hasInvalidInput() {
+        return this._inputList.some((inputElement) => {
+            return !inputElement.validity.valid;
+          });
+    }
+    
+    _toggleButtonState() {
+        if (this._hasInvalidInput(this._inputList)) {
+            this._buttonElement.classList.add(this._settings.inactiveButtonClass);
+            this._buttonElement.disabled = true;
+          } else {
+            this._buttonElement.classList.remove(this._settings.inactiveButtonClass);
+            this._buttonElement.disabled = false;
+          }
+    }
+
+    _setEventListeners() {
+        this._inputList = Array.from(
+            this._formElement.querySelectorAll(this._settings.inputSelector),
+          );
+          this._buttonElement = this._formElement.querySelector(
+            this._settings.submitButtonSelector,
+          );
+        
+          this._toggleButtonState();
+        
+          this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener("input", () => {
+                this._checkInputValidity(inputElement);
+                this._toggleButtonState();
+            });
+          });
+    }
+
+    enableValidation() {
+        this._formElement = document.querySelector(this._settings.formSelector);
+        this._formElement.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+          });
+
+        this._setEventListeners();
+    }
+}

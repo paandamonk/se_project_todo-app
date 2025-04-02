59281ea6fef587e004001a62767344a4d64f3304
Skye Emery <paandamonk@gmail.com>
2025-04-01 21:34:30 -0400

    added popup with PopupWithForm

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
-import Popup from "../components/Popup.js";
+import PopupWithForm from "../components/PopupWithForm.js";
 import Section from "../components/Section.js";
 
 //Instantiates the todosList as a new Section class.
@@ -16,12 +16,22 @@ const todosList = new Section({
 ".todos__list"
 );
 
-const addTodoPopup = new Popup("#add-todo-popup");
+const addTodoPopup = new PopupWithForm({popupSelector: '#add-todo-popup',
+  handlerFormSubmit: (getInputValues) => {
+    const { name, dateInput } = getInputValues;
+    const date = new Date(dateInput);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+    const id = uuidv4();
+    todosList.addItems({name, date, id});
+    addTodoPopup.close();
+    formValidator.resetValidation();
+  }
+});
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoForm = document.forms["add-todo-form"];
 //const addTodoPopup = document.querySelector("#add-todo-popup");
-//const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+//const addTodoCloseBtn = document.querySelector(".popup__close");
 
 const formValidator = new FormValidator(validationConfig, addTodoForm);
 
@@ -29,12 +39,9 @@ addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
 });
 
-/* addTodoCloseBtn.addEventListener("click", () => {
-  addTodoPopup.close();
-}); */
 
 //Adds new todos to the todoList.
-addTodoForm.addEventListener("submit", (evt) => {
+/* addTodoForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
 
   //grabs inputs from the form
@@ -51,7 +58,10 @@ addTodoForm.addEventListener("submit", (evt) => {
   todosList.addItems({name, date, id});
   addTodoPopup.close();
   formValidator.resetValidation();
-});
+}); */
 
+addTodoPopup.setEventListeners();
 todosList.renderItems();
 formValidator.enableValidation();
+
+export {formValidator};

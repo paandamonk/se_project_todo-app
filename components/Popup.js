95a8a81d163daeb313e35491bc262956ac271f82
Skye Emery <paandamonk@gmail.com>
2025-04-12 20:46:39 -0400

    removed some commented lines

--- components/Popup.js
+++ components/Popup.js
@@ -1,37 +1,34 @@
-
-//Popup class. Currently, only popup is when clicking the "Add Todo" button.
-//Instantiate the class when clicking "Add Todo" button.
 export default class Popup {
-    constructor({popupSelector}) {
-        this._popup = document.querySelector(popupSelector);
-        this._closeButton = this._popup.querySelector(".popup__close");
-        this._handleEscapeClose = this._handleEscapeClose.bind(this);
-    }
+  constructor({ popupSelector }) {
+    this._popup = document.querySelector(popupSelector);
+    this._closeButton = this._popup.querySelector(".popup__close");
+    this._handleEscapeClose = this._handleEscapeClose.bind(this);
+  }
 
-    open() {
-        this._popup.classList.add("popup_visible");
-        document.addEventListener("keyup", this._handleEscapeClose);
-    }
+  open() {
+    this._popup.classList.add("popup_visible");
+    document.addEventListener("keyup", this._handleEscapeClose);
+  }
 
-    close() {
-        this._popup.classList.remove("popup_visible");
-        document.removeEventListener("keyup", this._handleEscapeClose);
-    }
+  close() {
+    this._popup.classList.remove("popup_visible");
+    document.removeEventListener("keyup", this._handleEscapeClose);
+  }
 
-    _handleEscapeClose(evt) {
-        if(evt.key === "Escape") {
-            this.close();
-        };
+  _handleEscapeClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
     }
+  }
 
-    setEventListeners() {
-        this._closeButton.addEventListener("click", () => {
-            this.close();
-        });
-        this._popup.addEventListener("click", (evt) => {
-            if(evt.target === evt.currentTarget) {
-                this.close();
-            }
-        });
-    }
+  setEventListeners() {
+    this._closeButton.addEventListener("click", () => {
+      this.close();
+    });
+    this._popup.addEventListener("click", (evt) => {
+      if (evt.target === evt.currentTarget) {
+        this.close();
+      }
+    });
+  }
 }
--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,23 +1,23 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
-    constructor({popupSelector, handlerFormSubmit}) {
-        super({popupSelector});
-        this._handlerFormSubmit = handlerFormSubmit;
-        this._form = this._popup.querySelector("#add-todo-form");
-    }
+  constructor({ popupSelector, handlerFormSubmit }) {
+    super({ popupSelector });
+    this._handlerFormSubmit = handlerFormSubmit;
+    this._form = this._popup.querySelector("#add-todo-form");
+  }
 
-    _getInputValues(evt) {
-        const name = evt.target.name.value;
-        const dateInput = evt.target.date.value;
-        return ({name: name, dateInput: dateInput});
-    }
+  _getInputValues(evt) {
+    const name = evt.target.name.value;
+    const dateInput = evt.target.date.value;
+    return { name: name, dateInput: dateInput };
+  }
 
-    setEventListeners() {
-        super.setEventListeners();
-        this._form.addEventListener("submit", (evt) => {
-            evt.preventDefault();
-            this._handlerFormSubmit(this._getInputValues(evt));
-        });
-    }
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handlerFormSubmit(this._getInputValues(evt));
+    });
+  }
 }

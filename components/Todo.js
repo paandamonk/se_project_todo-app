5704c705791dc8b94db199d182f82299203eb115
Skye Emery <paandamonk@gmail.com>
2025-04-12 20:41:32 -0400

    added functionality of the todo counter todo counter now updates when checkbox is toggled, as well as when a todo is created or deleted.

--- components/Todo.js
+++ components/Todo.js
@@ -1,58 +1,63 @@
-
 //creates a to-do item with a name (and optionally, a due date) along with the appropriate handlers
 
 export default class Todo {
-    constructor(data, selector) {
-        this._data = data;
-        this._templateElement = document.querySelector(selector);
-    }
+  constructor(data, selector, handleCheck, handleDelete) {
+    this._completed = data.completed;
+    this._date = data.date;
+    this._name = data.name;
+    this._id = data.id;
+    this._templateElement = document.querySelector(selector);
+    this._handleCheck = handleCheck;
+    this._handleDelete = handleDelete;
+  }
 
-    //sets the necessary event listeners for the delete button and the checkbox.
-    _setEventListeners() {
-        this._todoCheckboxEl.addEventListener("change", () =>{
-            this._data.completed = !this._data.completed;
-        });
-        this._todoDeleteBtn.addEventListener("click", () => {
-            this._todoElement.remove();
-        });
-    }
+  //sets the necessary event listeners for the delete button and the checkbox.
+  _setEventListeners() {
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._completed = !this._completed;
+      this._handleCheck(this._completed);
+    });
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+      this._handleDelete(this._completed);
+    });
+  }
 
-    _generateCheckboxEl() {
-        this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-        this._todoLabel = this._todoElement.querySelector(".todo__label");
-        this._todoCheckboxEl.checked = this._data.completed;
-        this._todoCheckboxEl.id = `todo-${this._data.id}`;
-        this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
-    }
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._completed;
+    this._todoCheckboxEl.id = `todo-${this._id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._id}`);
+  }
 
-    //adds handles generating the markup from a template and adding the necessary data to the element
-    getView() {
-         this._todoElement = this._templateElement.content
-        .querySelector(".todo")
-        .cloneNode(true);
+  //adds handles generating the markup from a template and adding the necessary data to the element
+  getView() {
+    this._todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
 
-        this._todoNameEl = this._todoElement.querySelector(".todo__name");
-        this._todoDate = this._todoElement.querySelector(".todo__date");
-        this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    this._todoNameEl = this._todoElement.querySelector(".todo__name");
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
-        this._todoNameEl.textContent = this._data.name;
+    this._todoNameEl.textContent = this._name;
 
-        this._dueDate = new Date(this._data.date);
-        if (!isNaN(this._dueDate)) {
-            this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-            })}`;
+    this._dueDate = new Date(this._date);
+    if (!isNaN(this._dueDate)) {
+      this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
+        "en-US",
+        {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
         }
-
-        this._generateCheckboxEl();
-        this._setEventListeners();
-
-
-        
-        return this._todoElement;
+      )}`;
     }
 
+    this._generateCheckboxEl();
+    this._setEventListeners();
 
+    return this._todoElement;
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,48 @@ import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import Section from "../components/Section.js";
+import TodoCounter from "../components/TodoCounter.js";
+
+//Function to handle updating the todo counter through the checkboxes.
+function handleCheck(completed) {
+  todoCounter.updateCompleted(completed);
+}
+
+//Function to handle updating the todo counter through the delete button.
+function handleDelete(completed) {
+  if (completed) {
+    todoCounter.updateCompleted(false);
+  }
+  todoCounter.updateTotal(false);
+}
 
 //Instantiates the todosList as a new Section class.
-const todosList = new Section({
-  items: initialTodos,
-  renderer: (data) => {
-    const todo = new Todo(data, "#todo-template");
-    return todo.getView();
-  }
-},
-".todos__list"
+const todosList = new Section(
+  {
+    items: initialTodos,
+    renderer: (data) => {
+      const todo = new Todo(data, "#todo-template", handleCheck, handleDelete);
+      return todo.getView();
+    },
+  },
+  ".todos__list"
 );
 
-const addTodoPopup = new PopupWithForm({popupSelector: '#add-todo-popup',
+//Instantiate the todoCounter.
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
+
+const addTodoPopup = new PopupWithForm({
+  popupSelector: "#add-todo-popup",
   handlerFormSubmit: (getInputValues) => {
     const { name, dateInput } = getInputValues;
     const date = new Date(dateInput);
     date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
     const id = uuidv4();
-    todosList.addItems({name, date, id});
+    todosList.addItems({ name, date, id });
     addTodoPopup.close();
+    todoCounter.updateTotal(true);
     formValidator.resetValidation();
-  }
+  },
 });
 
 const addTodoButton = document.querySelector(".button_action_add");
@@ -38,7 +58,6 @@ const formValidator = new FormValidator(validationConfig, addTodoForm);
 addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
 });
-
 
 //Adds new todos to the todoList.
 /* addTodoForm.addEventListener("submit", (evt) => {
@@ -64,4 +83,4 @@ addTodoPopup.setEventListeners();
 todosList.renderItems();
 formValidator.enableValidation();
 
-export {formValidator};
+export { formValidator };
